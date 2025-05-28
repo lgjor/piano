550235c35e3a7aa3c17effaa50b79cb3e45a0fd7
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-05-28 17:12:55 -0300

    Fixed Uncaught (in promise) AbortError: The play() request was interrupted by a new load request

--- src/script/engine.js
+++ src/script/engine.js
@@ -1,12 +1,17 @@
 const audioPath = "src/tunes/";
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
+const volumeSlider = document.querySelector('.volume-slider input');
+const keysCheck = document.querySelector('.keys-check input');
 
-let mapedKeys = [];
+const mapedKeys = [];
+
+let audio = new Audio;
 
 const playTune = (key) => {
-  let audio = new Audio;
+  const audio = new Audio(); // Create a new Audio object each time
   audio.type = "audio/wav";
   audio.src = `${audioPath}${key}.wav`;
+  audio.volume = volumeSlider.value; // Set the volume of the new audio object
   audio.play();
   const clickedKey = document.querySelector(`.key[data-key="${key}"]`);
   clickedKey.classList.add("active");
@@ -20,10 +25,25 @@ pianoKeys.forEach((key) => {
     mapedKeys.push(key.dataset.key);
 });
 
-document.addEventListener(
-  "keydown",
-  (event) => { 
+// Function to update the keydown event listener
+const updateKeydownListener = () => {
+    document.removeEventListener("keydown", keydownHandler); // Remove the old listener
+
+    document.addEventListener("keydown", keydownHandler); // Add the new listener
+};
+
+// Keydown handler function
+function keydownHandler(event) {
     if (mapedKeys.includes(event.key)) {
-      playTune(event.key)
-    };
-});
+        playTune(event.key);
+    }
+}
+
+// Initial keydown listener setup
+updateKeydownListener();
+
+const showHideKeys = () => {
+  pianoKeys.forEach(key => key.classList.toggle("hide"));
+}
+
+keysCheck.addEventListener("click", showHideKeys);
