9d983ae3d803c019cd830ab2e1484dd0f83230bc
Lucas Godoy <lucasgodoyjor@gmail.com>
2025-05-28 16:18:48 -0300

    Mudado layout para qwerty e adicionado controle de teclas mapeadas

--- src/script/engine.js
+++ src/script/engine.js
@@ -1,19 +1,29 @@
 const audioPath = "src/tunes/";
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
 
+let mapedKeys = [];
+
 const playTune = (key) => {
   let audio = new Audio;
   audio.type = "audio/wav";
   audio.src = `${audioPath}${key}.wav`;
   audio.play();
+  const clickedKey = document.querySelector(`.key[data-key="${key}"]`);
+  clickedKey.classList.add("active");
+  setTimeout(() => {
+    clickedKey.classList.remove("active");
+  }, 150);
 }
 
 pianoKeys.forEach((key) => {
     key.addEventListener("click", () => playTune(key.dataset.key));
+    mapedKeys.push(key.dataset.key);
 });
 
 document.addEventListener(
   "keydown",
   (event) => { 
-    playTune(event.key);
+    if (mapedKeys.includes(event.key)) {
+      playTune(event.key)
+    };
 });
